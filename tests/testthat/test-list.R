6a1b7f32bf8ad92cf299f948c471eae59ff5e37b
Paweł Biernat <pawel.biernat@comma-soft.de>
2019-08-21 13:07:45 +0200

    Updated the test data

--- tests/testthat/test-list.R
+++ tests/testthat/test-list.R
@@ -4,5 +4,5 @@ DATADIR <- "../data/readers/"
 
 test_that("Check the list function", {
     dsets_list <- fgread::list_datasets(data_dir=DATADIR)
-    expect_equal(length(dsets_list), 5)
+    expect_equal(length(dsets_list), 7)
 })
