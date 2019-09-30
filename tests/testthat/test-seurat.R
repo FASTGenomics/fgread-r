5f1aae98087fcc6947f7f075ba2cd61af89252b3
PapenMi <Michael.von.Papen@comma-soft.com>
2019-09-30 14:51:07 +0200

    adapted to new test data

--- tests/testthat/test-seurat.R
+++ tests/testthat/test-seurat.R
@@ -8,7 +8,9 @@ DATASETS <- list(
     list(id=2, dim=c(33538, 1222), title="Seurat Object dataset", format="Seurat Object"),
     list(id=3, dim=c(20, 10), title="AnnData dataset", format="AnnData"),
     list(id=4, dim=c(33538, 1222), title="10x (hdf5) dataset", format="10x (hdf5)"),
-    list(id=5, dim=c(99, 20000), title="Drop-Seq (tsv) dataset", format="Drop-Seq (tsv)")
+    list(id=5, dim=c(99, 20000), title="tab-separated text dataset", format="tab-separated text"),
+    list(id=10, dim=c(99, 499), title="comma-separated text dataset", format="comma-separated text"),
+    list(id=11, dim=c(99, 499), title="tab-separated text variant dataset", format="tab-separated text")
 )
 
 Map(
