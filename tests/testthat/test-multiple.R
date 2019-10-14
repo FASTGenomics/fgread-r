60b4c6e8d825b503085c7c409926abea04b20cfb
Benedikt Reiz <41583627+LustigePerson@users.noreply.github.com>
2019-10-14 13:41:38 +0200

    Dev br (#4)
    Add blog reference, add experimental readers ,and make read_Datasets compatible with single datasets

--- tests/testthat/test-multiple.R
+++ tests/testthat/test-multiple.R
@@ -5,7 +5,7 @@ DATADIR <- "../data/readers"
 test_that("Loading multiple datasets", suppressWarnings({
     dsets_list <- fgread::get_datasets(data_dir=DATADIR)
     sel <- c(1,2,3)
-    dsets <- fgread::read_datasets(dsets_list[sel])
+    dsets <- fgread::read_datasets(dsets_list[sel], experimental_readers=T)
 
     expect_equal(length(dsets), length(sel))
     print(names(dsets))
