643411e96022a53625e3ccc1f1b7423ad6f3d624
Heikamp <kathrin.heikamp@comma-soft.com>
2020-03-11 12:57:21 +0100

    Tests for old and new functions

--- tests/testthat/test-list.R
+++ tests/testthat/test-list.R
@@ -1,8 +1,18 @@
-context("Listing datasets")
+context("Listing datasets using ds_info()")
 
 DATADIR <- "../data/all_datasets/"
 
-test_that("Check the list function", {
-    dsets_list <- fgread::get_datasets(data_dir=DATADIR)
-    expect_equal(length(dsets_list), 12)
+test_that("Check the info function", {
+    data_table <- fgread::ds_info(data_dir=DATADIR, pretty=FALSE)
+    expect_equal(dim(data_table)[1], 12)
+})
+
+test_that("Check the info function for one dataset", {
+    data_table <- fgread::ds_info('Loom dataset', data_dir=DATADIR, pretty=FALSE)
+    expect_equal(dim(data_table)[1], 1)
+})
+
+test_that("Check the info function for one dataset", {
+    data_table <- fgread::ds_info('dataset-f93640431b0e4835a55332r9ii3nzffn', data_dir=DATADIR, pretty=FALSE)
+    expect_equal(dim(data_table)[1], 1)
 })
---
+++ tests/testthat/test-list_old.R
@@ -0,0 +1,8 @@
+context("Listing datasets using get_datasets()")
+
+DATADIR <- "../data/all_datasets/"
+
+test_that("Check the list function", {
+    dsets_list <- fgread::get_datasets(data_dir=DATADIR)
+    expect_equal(length(dsets_list), 12)
+})
