09abf08aae512532523b23ae2b1e15f68374d10a
mvonpapen <26275671+mvonpapen@users.noreply.github.com>
2020-06-24 16:57:59 +0200

    workaround for newest anndata version

--- R/readers.R
+++ R/readers.R
@@ -113,7 +113,20 @@ read_anndata_to_seurat <- function(ds_file) {
   warning(glue::glue('!!Importing AnnData is not always working as expected in Seurat v3 .',
         'You can use our FASTGenomics experimental reader by setting "experimental_readers=TRUE" in `load_data`. ',
         'For more information please see {DOCSURL}.'))
-  return(Seurat::ReadH5AD(ds_file))
+  seurat = tryCatch({
+      seurat <- Seurat::ReadH5AD(ds_file)
+  }, error = function(e) {
+      warning(glue::glue('Import of newest Anndata version requires to install the experimental `SeuratDisk` library from github.\n')
+      if (!requireNamespace("remotes", quietly = TRUE)) {
+          install.packages("remotes")
+      }
+      remotes::install_github("mojaveazure/seurat-disk")
+      library(SeuratDisk)
+      print(glue::glue('Converting to intermediate file `/fastgenomics/analysis/data_intermediate.h5seurat`.'))
+      Convert(ds_file, dest = "/fastgenomics/analysis/data_intermediate.h5seurat", overwrite = F)
+      seurat <- LoadH5Seurat("/fastgenomics/analysis/data_intermediate.h5seurat")
+  })
+  return(seurat)
 }
 
 
